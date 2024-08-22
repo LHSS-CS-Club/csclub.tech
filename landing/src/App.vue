5d8ebf1d9f02a51df8f1a04f61dc70143ae1009e
jeffrey-zang <jeffrey.zang.07@gmail.com>
2024-08-21 22:15:53 -0400

    task(landing): finish landing page

--- landing/src/App.vue
+++ landing/src/App.vue
@@ -1,30 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div :class="{ 'dark': isDarkMode }">
+  <div class="grid place-items-center h-screen bg-light dark:bg-dark">
+      <div class="flex flex-col justify-center text-center max-w-[60rem] text-dark dark:text-light">
+        <button @click="toggleDark" class="cursor-default">
+          <img src="./assets/cs.jpeg" class="logo h-[40vh] mx-auto rounded-3xl mb-12 cursor-pointer" alt="logo" />
+        </button>
+        <h3 class="text-4xl font-bold mb-2">Laurel Heights Computer Science Club</h3>
+        <p class="text-sm">
+          A group of students from Laurel Heights Secondary School dedicated to creating opportunities for students to learn more about computer science and technology.
+        </p>
+        <div class="flex gap-4 text-xl justify-center mt-2">
+          <button class="flex items-center gap-2 hover:opacity-60 transition-opacity">
+            <img src="./assets/icons/File.svg" class="w-6 h-6" alt="File icon" />
+            Google Classroom
+          </button>
+          <button class="flex items-center gap-2 hover:opacity-60 transition-opacity">
+            <img src="./assets/icons/GitHub Logo.svg" class="w-6 h-6 " alt="GitHub icon" />
+            GitHub
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDarkMode: false
+    }
+  },
+  methods: {
+    toggleDark() {
+      this.isDarkMode = !this.isDarkMode;
+    },
+  }
+}
+</script>
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
   will-change: filter;
-  transition: filter 300ms;
+  transition: all 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+  height: 41vh;
 }
 </style>
---
+++ landing/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: "#EFEFEF",
+        dark: "#212529"
+      }
+    },
+  },
+  plugins: [],
+}
